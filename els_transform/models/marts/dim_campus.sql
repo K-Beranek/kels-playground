217c6565813a4bea78d677ed7b0832a8cc9eb890
Karel Beranek <karel.beranek@volny.cz>
2026-09-02 00:23:03 +0200

    Missed fixes?

--- els_transform/models/marts/dim_campus.sql
+++ els_transform/models/marts/dim_campus.sql
@@ -1,7 +1,7 @@
 -- Use the `ref` function to select from other models
 
 select
-    {{ dbt_utils.generate_surrogate_key(['campus.campus_id']) }} as campus_dim_id,
+    {{ dbt_utils.generate_surrogate_key(['campus.campus_id']) }} as campus_dim_key,
     campus_id,
     uuid,
     code,
